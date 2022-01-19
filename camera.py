d2bb15012bf13167a97a47156bafb429acec63df
Hank Stennes <hpstennes@gmail.com>
2022-01-18 20:03:26 -0600

    remove unnecessary files

--- camera.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-from datetime import datetime
-
-now = datetime.now()
-dt_string = now.strftime("%d-%m-%Y-H:%M:%S")
-camera = PiCamera()
-camera.capture(dt_string + ".png")
-camera.close()
