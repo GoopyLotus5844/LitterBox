6a3ea57520cab725734b073d8a1d3b9943f2befb
Hank Stennes <hpstennes@gmail.com>
2022-01-18 21:26:31 -0600

    remove test file

--- test.py
+++
@@ -1,7 +0,0 @@
-import logging
-import time
-from datetime import datetime as date_util
-
-logging.basicConfig(filename='sms_server.log', level=logging.INFO)
-
-logging.info('SMS recieved at time %s', date_util.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
